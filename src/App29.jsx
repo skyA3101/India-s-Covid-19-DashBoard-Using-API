36c943f3ffb06ca56c7fdc3ec4e89fb7b7ce0120
skyA3101 <105971170+skyA3101@users.noreply.github.com>
2022-09-27 11:42:38 +0530

    Add files via upload

---
+++ src/App29.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Statewise from "./Component/StateWiseData/statewise";
+
+const App29 = () => {
+    return (
+        <>
+            <Statewise/>
+        </>
+    )
+}
+
+export default App29;
